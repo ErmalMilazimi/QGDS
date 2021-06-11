4c2b9a0c8936ae10a34041855b19efcb9bfa88ea
Ermal Milazimi <milazimi.ermal@gmail.com>
2021-06-11 11:47:54 +0200

    loader changed, header changed, navbar with observer

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,32 @@
 const loader = document.querySelector(".loader");
 window.addEventListener("load", () => {
-  loader.parentElement.removeChild(loader);
+  setTimeout(function () {
+    loader.parentElement.removeChild(loader);
+  }, 10);
 });
+
+const sectionOne = document.querySelector("header");
+const navLogo = document.querySelector(".logo");
+const nav = document.querySelector("nav");
+
+const sectionOneOptions = {
+  rootMargin: "-150px 0px 0px 0px",
+};
+
+const sectionOneObesrver = new IntersectionObserver(function (
+  entries,
+  sectionOneOptions
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      navLogo.classList.add("active");
+      nav.classList.add("active");
+    } else {
+      navLogo.classList.remove("active");
+      nav.classList.remove("active");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObesrver.observe(sectionOne);
