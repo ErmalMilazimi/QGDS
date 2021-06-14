27d99cb4d9197707321461d59f3b6fed739f8965
Ermal Milazimi <milazimi.ermal@gmail.com>
2021-06-14 16:23:21 +0200

    responsive fixed, added aboutsection

--- dist/js/main.js
+++ dist/js/main.js
@@ -47,3 +47,28 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("open");
   navOpen.classList.toggle("open");
 });
+
+const scrollFadeIn = document.querySelectorAll(".scroll-fade-in");
+const fadeInOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -50px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("active");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+fadeInOptions);
+
+scrollFadeIn.forEach((element) => {
+  appearOnScroll.observe(element);
+});
